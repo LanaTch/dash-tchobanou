4bece69d61afa31beeb57ea05aed2540aa3e2a58
LanaTch <44633700+LanaTch@users.noreply.github.com>
2022-03-15 22:12:11 +0300

    edit params
    edit params for linux and true requirements

--- games_market_dash_Svetlana_Tchobanou.py
+++ games_market_dash_Svetlana_Tchobanou.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
-path_data  = Path(Path(__file__).parent, 'games.csv')
+path_data = Path(Path(__file__).parent, 'games.csv')
 
 df = pd.read_csv(path_data)
 df.dropna(inplace=True)
@@ -18,42 +18,42 @@ df = df[df['Year_of_Release'] >= 2000]
 df['Year_of_Release'] = df['Year_of_Release'].astype(int)
 
 years_filter = dcc.RangeSlider(
-                    df['Year_of_Release'].min(),
-                    df['Year_of_Release'].max(),
+                    id='crossfilter-year--slider',
+                    min=df['Year_of_Release'].min(),
+                    max=df['Year_of_Release'].max(),
                     step=1, 
                     pushable=False,
-                    id='crossfilter-year--slider',
                     value=[df['Year_of_Release'].min(), df['Year_of_Release'].max()],
                     marks={str(year): str(year) for year in df['Year_of_Release'].unique()}
                     )
 
 genres_filter = dcc.Dropdown(
-                    df['Genre'].unique(),
-                    df['Genre'].unique(),
                     id='crossfilter-genres',
+                    options=dict(zip(df['Genre'].unique(), df['Genre'].unique())),
+                    value=df['Genre'].unique(),
                     multi=True
                     )    
    
 ratings_filter = dcc.Dropdown(
-                    df['Rating'].unique(),
-                    df['Rating'].unique(),
                     id='crossfilter-ratings',
+                    options=dict(zip(df['Rating'].unique(), df['Rating'].unique())),
+                    value=df['Rating'].unique(),
                     multi=True
                      )    
     
-graf1= dcc.Graph(id='stacked-area1')
+graf1 = dcc.Graph(id='stacked-area1')
 graf2 = dcc.Graph(id='scatter-plot2')
 
 app.layout = html.Div([
-    html.H2(children="Интерактивный дашборд. Статистика по играм.", style={'padding': '10px 5px 5px 5px', "fontSize": "32px"}), #заголовок    
+    html.H2(children="Интерактивный дашборд. Статистика по играм.",
+            style={'padding': '10px 5px 5px 5px', "fontSize": "32px"}),
     
-    html.Label("Предназначен для обзора статистики по играм, выпущенным с 2000 по 2016 год."
-                                "Фильтры по жанрам и рейтингам поддерживают множественный выбор, фильт по годам выпуска (внизу страницы) интервальный.",
- 
+    html.Div("Предназначен для обзора статистики по играм, выпущенным с 2000 по 2016 год."
+            "Фильтры по жанрам и рейтингам поддерживают множественный выбор, "
+            "фильт по годам выпуска (внизу страницы) интервальный.",
              style={'padding': '10px 5px 5px 5px',"fontSize": "24px" }), #инструкция
     
     html.Div([
-
         html.Div("Фильтр жанров",style={'width': '49%', 'display': 'inline-block',"fontSize": "20px"}),
         html.Div("Фильтр рейтингов", style={'width': '49%', 'float': 'right', 'display': 'inline-block',"fontSize": "20px"})
 
@@ -90,15 +90,19 @@ app.layout = html.Div([
 
 
 @app.callback(
-    Output('stacked-area1', 'figure'),
-    Output('scatter-plot2', 'figure'),
-    Output('interactive-text1', 'children'),
-    Input('crossfilter-genres', 'value'),
-    Input('crossfilter-ratings', 'value'),
-    Input('crossfilter-year--slider', 'value'))
-def update_data(genre_values, 
-                 rating_values,
-                 year_values):
+    [
+        Output('stacked-area1', 'figure'),
+        Output('scatter-plot2', 'figure'),
+        Output('interactive-text1', 'children')
+    ],
+    [
+        Input('crossfilter-genres', 'value'),
+        Input('crossfilter-ratings', 'value'),
+        Input('crossfilter-year--slider', 'value')
+    ])
+def update_data(genre_values,
+                rating_values,
+                year_values):
     list_years = [i for i in range(year_values[0], year_values[1] + 1)]
     dff = df.query('Year_of_Release in @list_years and Rating in @rating_values and Genre in @genre_values')
 
@@ -106,15 +110,16 @@ def update_data(genre_values,
     fig2['layout']['autosize'] = True
             
     data = dff.groupby(by=['Platform', 'Year_of_Release'],  as_index=False)\
-              .agg({'Name' : 'count'})\
-              .rename(columns={'Name':'num'})
+              .agg({'Name': 'count'})\
+              .rename(columns={'Name': 'num'})
     
     fig1 = go.Figure()
 
     for cur_plat in list(data.Platform.unique()):
 
         fig1.add_trace(go.Scatter(
-            x=data.query('Platform == @cur_plat')['Year_of_Release'].values, y=data.query('Platform == @cur_plat')['num'].values,
+            x=data.query('Platform == @cur_plat')['Year_of_Release'].values,
+            y=data.query('Platform == @cur_plat')['num'].values,
             mode='lines',
             line=dict(width=0.5),
             name=cur_plat,
@@ -128,7 +133,7 @@ def update_data(genre_values,
 
     
 if __name__ == '__main__':
-    app.run_server(port='8050')
+    app.run_server(debug=True)
     
     
     
